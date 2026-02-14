07b209b639d0c5a43ef50a0b459fbaa2b0857703
toha-gornich <92648253+toha-gornich@users.noreply.github.com>
2026-02-14 23:36:35 +0200

    create project

---
+++ UIKitLifecycleLab/UIKitLifecycleLab/UIKitLifecycleLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitLifecycleLabApp.swift
+//  UIKitLifecycleLab
+//
+//  Created by Горніч Антон on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct UIKitLifecycleLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
